caccf9a761608bd1bd09fc6fcffeb4784db6a87e
MichaelGrupp <mg91@arcor.de>
2015-07-20 17:32:00 +0200

    median filter is faster now

--- core/medianFilter.m
+++ core/medianFilter.m
@@ -1,27 +1,27 @@
 function [imgOut] = medianFilter(img, fSize)
+
 [m,n] = size(img);
 imgOut = zeros(m,n);
 kernel = ones(fSize)/fSize^2;
 w = round(fSize/2);
 
 for i=1:m
+    iOffset1=w; iOffset2=w;
+    if(i-w<1)
+    iOffset1=w+(i-w)-1;
+    end
+    if(i+w>m)
+        iOffset2=jOffset1-((i+w)-m);
+    end
     for j=1:n
-        iOffset1=w; iOffset2=w; jOffset1=w; jOffset2=w;
-        if(i-w<1)
-            iOffset1=w+(i-w)-1;
-        end
-        if(i+w>m)
-            iOffset2=jOffset1-((i+w)-m);
-        end
+        jOffset1=w; jOffset2=w;
         if(j-w<1)
             jOffset1=w+(j-w)-1;
         end
         if(j+w>n)
             jOffset2=jOffset2-((j+w)-n);
         end
-        adjacency = conv2(...
-            img(i-iOffset1:i+iOffset2,j-jOffset1:j+jOffset2), ...
-            kernel, 'same');
+        adjacency = img(i-iOffset1:i+iOffset2,j-jOffset1:j+jOffset2);
         stacked = adjacency(:);
         stacked = sort(stacked);
         median = stacked(round(end/2));
--- demoFilters.m
+++ demoFilters.m
@@ -1,8 +1,9 @@
 addpath('core')
+profile on;
 
 close all;
 %works only with double!!
-img = im2double(imread('testImages/test.jpg'));
+img = im2double(imread('testImages/lenna.jpg'));
 if (ndims(img)==3)
     img=rgb2gray(img);
 end
@@ -19,3 +20,5 @@ subplot(2,2,1), subimage(img), title('original')
 subplot(2,2,2), subimage(imgOutMF), title('mean filtered')
 subplot(2,2,3), subimage(imgOutGF), title('gaussian filtered')
 subplot(2,2,4), subimage(imgOutME), title('median filtered')
+
+profile viewer
